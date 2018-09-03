adaa47ca787607e1cddfefed94a192708da47dbd
ucev <zhangshuaiyf@icloud.com>
2018-09-03 10:19:39 +0800

    import setup workflow, auto-config base.config.js

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 if [ -e app ]
 then
+if [ -e app/config/base.config.js ]
+then
+cp app/config/base.config.js base.config.js
+fi
 rm -rf app
 fi
 
@@ -39,8 +43,18 @@ docdir=${docdir//\//\\\/}
 mysqldir=${mysqldir//\//\\\/}
 pass=${pass//\//\\\/}
 
-
+# create docker config files
 for file in `ls ./template`
 do
 sed "s/MY_TEMP_DIR/$tmpdir/g;s/MY_IMAGE_DIR/$imagedir/g;s/MY_DOCS_DIR/$docdir/g;s/MY_MYSQL_DIR/$mysqldir/g;s/MY_MYSQL_PASS/$pass/g" ./template/$file > $file
 done
+
+# setup website config
+if [ -e base.config.js ]
+then
+mv base.config.js app/config/base.config.js
+else
+sed "s/host:\ \"\"/host:\ \"mysql\"/g;s/user:\ \"\"/user:\ \"root\"/g;s/password:\ \"\"/password:\ \"${pass}\"/g;s/database:\ \"\"/database:\ \"blog_node\"/g" app/config/base.config.origin.js > app/config/base.config.js
+printf "SETUP FINISH\n"
+printf "\033[1;37;42mNOTICE: remember to modify app/config/base.origin.js\033[0m\n"
+fi
---
+++ wait-for-mysql.js
@@ -0,0 +1,58 @@
+const mysql = require('promise-mysql')
+const process = require('process')
+const { spawn } = require('child_process')
+
+
+function connectmysql() {
+    return new Promise((resolve, reject) => {
+        function __conn() {
+            mysql.createConnection({
+                host: 'mysql',
+                user: 'root',
+                password: '1234567890'
+            }).then(() => {
+                console.log('MySQL is up')
+                return resolve()
+            }).catch((err) => {
+                console.log('MySQL is unavailable')
+                setTimeout(__conn, 1000)
+            })            
+        }
+        __conn()
+    })
+}
+
+async function runChildProcess() {
+    const argv = process.argv.slice(2)
+    if (argv.length == 0) {
+        return
+    }
+    let cprocess
+    if (argv.length == 1) {
+        cprocess = spawn(argv[0])
+    } else {
+        cprocess = spawn(argv[0], argv.slice(1))
+    }
+    return new Promise((resolve, reject) => {
+        cprocess.stdout.on('data', (data) => {
+            console.log(`${argv[0]} -- ${data}`)
+        })
+        cprocess.stderr.on('data', (data) => {
+            console.log(`${argv[0]} -- ${data}`)
+        })
+        cprocess.on('close', () => {
+            resolve()
+        })
+        cprocess.on('error', () => {
+            reject()
+        })
+    })
+}
+console.log(process.argv)
+
+connectmysql().then(() => {
+    return runChildProcess()
+}).then(() => {
+    // spawn('npm', ['start'])
+    console.log('--------- end ---------')
+})
